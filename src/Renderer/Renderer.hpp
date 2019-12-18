125c99ce950c963e8731b4e94727f23764fa7ec2
Peter <petercrabbe92@gmail.com>
2019-12-18 17:47:46 +0800

    Fix linker errors

---
+++ src/Renderer/Renderer.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <utility>
+#include <vector>
+
+#include "Models/mesh.hpp"
+#include "Shader.hpp"
+
+class Renderer {
+  public:
+    Renderer();
+    ~Renderer();
+    void draw(const glm::mat4 &view, const glm::mat4 &projection);
+    static void addToDraw(const glm::mat4 &modelMesh, const size_t &modelID);
+    static auto drawList() -> std::vector<std::pair<glm::mat4, size_t>> &;
+
+  private:
+    Shader *shader = nullptr;
+};
