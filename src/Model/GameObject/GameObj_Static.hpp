95ef7d65a75d99cba2b00ce9488757f4b70c405f
William Vlahos <wxavierv@gmail.com>
2020-04-13 16:05:43 +0800

    Fixed various compilation and logic bugs for GameObj_Static. Properly implemented inherited constructor parameter value passing.

--- src/Model/GameObject/GameObj_Static.hpp
+++ src/Model/GameObject/GameObj_Static.hpp
@@ -18,13 +18,13 @@ public:
 		/*!
 		 * @brief Sets initial class contents and calls the inherited class constructor.
 		 * @param [in] path The path to a target external model file for this GameObject.
+		 * @param [in] physBody The identifier for the physical body for this GameObject.
 		 * @param [in] position The position of the GameObject in the 3D environment.
 		 * @param [in] look_at The point the GameObject is looking at, relative to its position.
 		 * @param [in] look_up The point where GameObject is looking up, relative to its position.
 		 * @see GameObject_Base
 		 */
-	GameObj_Static(std::string path, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up)
-		: GameObject_Base(path, position, look_at, look_up);
+	GameObj_Static(std::string path, unsigned long int physBody, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up);
 
 		//! GameObj_Static destructor
 	~GameObj_Static();
@@ -34,7 +34,7 @@ public:
 		 * @return A unique identifying value for the GameObj_Static class.
 		 * @warning May return 0 if an error occured with registering the ID!
 		 */
-	void gameObj_getType() const;
+	GameObjectType gameObj_getType() const;
 
 		//! Adds the GameObject to the draw queue.
 	void gameObj_addToDraw();
@@ -45,5 +45,5 @@ private:
 		 * @return A copy of the unique and static identifying value for GameObj_Static.
 		 * @warning May return 0 if an error occured with registering the ID!
 		 */
-	static GameObjectType gameObj_getTypeID() const;
+	static GameObjectType gameObj_getTypeID();
 };
--- src/Model/GameObject/detail/GameObj_Static.cpp
+++ src/Model/GameObject/detail/GameObj_Static.cpp
@@ -1,11 +1,13 @@
 	/// Definition Include
 #include "../GameObj_Static.hpp"
 
-GameObj_Static::GameObj_Static() {
+GameObj_Static::GameObj_Static()
+	: GameObject_Base() {
 	// GameObj_Static has no unique attributes to initialise yet
 }
 
-GameObj_Static::GameObj_Static(std::string path, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up) {
+GameObj_Static::GameObj_Static(std::string path, unsigned long int physBody, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up)
+	: GameObject_Base(path, physBody, position, look_at, look_up) {
 	// GameObj_Static has no unique attributes to initialise yet
 }
 
@@ -22,9 +24,9 @@ void GameObj_Static::gameObj_addToDraw() {
 	//TODO: Implement this stubbed out function
 }
 
-GameObjectType GameObj_Static::gameObj_getTypeID() const {
+GameObjectType GameObj_Static::gameObj_getTypeID() {
 	// Keep track of this class's identifier for its type
-	static const GameObjectType classID = gameObj_getType();
+	static const GameObjectType classID = gameObj_registerTypeID();
 
 	// Return a copy of the tracked id
 	return classID;
