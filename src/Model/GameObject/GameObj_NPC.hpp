022af0ad71bda65b81ab204c1af65f3f447bb4e9
William Vlahos <wxavierv@gmail.com>
2020-04-13 16:11:28 +0800

    Fixed various compilation and logic bugs for GameObj_NPC. Properly implemented inherited constructor parameter value passing.

--- src/Model/GameObject/GameObj_NPC.hpp
+++ src/Model/GameObject/GameObj_NPC.hpp
@@ -18,13 +18,13 @@ public:
 		/*!
 		 * @brief Sets initial class contents and calls the inherited class constructor.
 		 * @param [in] path The path to a target external model file for this GameObject.
+		 * @param [in] physBody The identifier for the physical body for this GameObject.
 		 * @param [in] position The position of the GameObject in the 3D environment.
 		 * @param [in] look_at The point the GameObject is looking at, relative to its position.
 		 * @param [in] look_up The point where GameObject is looking up, relative to its position.
 		 * @see GameObject_Base
 		 */
-	GameObj_NPC(std::string path, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up)
-		: GameObject_Base(path, position, look_at, look_up);
+	GameObj_NPC(std::string path, unsigned long int physBody, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up);
 
 		//! GameObj_NPC destructor
 	~GameObj_NPC();
@@ -45,5 +45,5 @@ private:
 		 * @return A unique identifying value for the GameObj_NPC class.
 		 * @warning May return 0 if an error occured with registering the ID!
 		 */
-	static GameObjectType gameObj_getTypeID() const;
+	static GameObjectType gameObj_getTypeID();
 };
--- src/Model/GameObject/detail/GameObj_NPC.cpp
+++ src/Model/GameObject/detail/GameObj_NPC.cpp
@@ -1,11 +1,13 @@
 	/// Definition Include
 #include "../GameObj_NPC.hpp"
 
-GameObj_NPC::GameObj_NPC() {
+GameObj_NPC::GameObj_NPC()
+	: GameObject_Base() {
 	// GameObj_NPC has no unique attributes to initialise yet
 }
 
-GameObj_NPC::GameObj_NPC(std::string path, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up) {
+GameObj_NPC::GameObj_NPC(std::string path, unsigned long int physBody, glm::vec3 position, glm::vec3 look_at, glm::vec3 look_up)
+	: GameObject_Base(path, physBody, position, look_at, look_up) {
 	// GameObj_NPC has no unique attributes to initialise yet
 }
 
@@ -22,9 +24,9 @@ void GameObj_NPC::gameObj_addToDraw() {
 	//TODO: Implement this stubbed out function
 }
 
-GameObjectType GameObj_NPC::gameObj_getTypeID() const {
+GameObjectType GameObj_NPC::gameObj_getTypeID() {
 	// Keep track of this class's identifier for its type
-	static const GameObjectType classID = gameObj_getType();
+	static const GameObjectType classID = gameObj_registerTypeID();
 
 	// Return a copy of the tracked id
 	return classID;
