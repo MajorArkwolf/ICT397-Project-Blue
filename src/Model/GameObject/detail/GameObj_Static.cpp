bf8b199a8a578194b15e345d75983c26d05409fe
William Vlahos <wxavierv@gmail.com>
2020-04-15 19:16:14 +0800

    Fixed incorrect translations made in the gameObj_addToDraw() call.

--- src/Model/GameObject/detail/GameObj_Static.cpp
+++ src/Model/GameObject/detail/GameObj_Static.cpp
@@ -35,14 +35,14 @@ GameObjType GameObj_Static::_gameObj_getTypeID() {
 void GameObj_Static::gameObj_addToDraw() {
 	// Prepare for model position configuration
 	glm::mat4 translation = glm::mat4(1.0f);
-	
-	// Configure the model orientation
-	translation = glm::rotate(translation, glm::radians(gameObj_rotateTilt), glm::vec3(0.0f, 0.0f, 1.0f));
-	translation = glm::rotate(translation, glm::radians(gameObj_rotateVert), glm::vec3(1.0f, 0.0f, 0.0f));
-	translation = glm::rotate(translation, glm::radians(gameObj_rotateHori), glm::vec3(0.0f, 1.0f, 0.0f));
 
 	// Configure the model position
 	translation = glm::translate(translation, gameObj_pos);
+
+	// Configure the model orientation
+	translation = glm::rotate(translation, glm::radians(gameObj_rotateHori), glm::vec3(0.0f, 1.0f, 0.0f));
+	translation = glm::rotate(translation, glm::radians(gameObj_rotateVert), glm::vec3(1.0f, 0.0f, 0.0f));
+	translation = glm::rotate(translation, glm::radians(gameObj_rotateTilt), glm::vec3(0.0f, 0.0f, 1.0f));
 
 	// Add the GameObject's model to the renderer's draw call
 	Renderer::addToDraw(translation, gameObj_getModelID());
