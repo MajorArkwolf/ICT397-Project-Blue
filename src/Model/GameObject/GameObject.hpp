525c9599c46e0a54f9dea71adadaddde9d6df8f2
William Vlahos <wxavierv@gmail.com>
2020-04-15 16:50:33 +0800

    Added a very basic header file to simplify GameObject includes.

---
+++ src/Model/GameObject/GameObject.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+	/// The base GameObject
+#include "GameObj_Base.hpp"
+
+	/// All child GameObjects
+#include "GameObj_Static.hpp"
+#include "GameObj_Player.hpp"
+#include "GameObj_NPC.hpp"
+
+	/// All GameObject managers
+#include "GameAssetFactory.hpp"
