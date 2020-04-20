b3a4f83d98ecb7f85599d53009b3f1f856c0993f
William Vlahos <wxavierv@gmail.com>
2020-04-20 12:16:00 +0800

    Added safety checks (and exception throwing) to the LuaHelper GameObject. Added missing doxygen to the LuaHelper GameObject. Removed unused code and includes from the LuaHelper GameObject.

--- src/Model/GameObject/LuaHelper.hpp
+++ src/Model/GameObject/LuaHelper.hpp
@@ -5,62 +5,155 @@
 
 	/// Internal Dependencies
 #include "GameObject.hpp"
-#include "Static.hpp"
-#include "Player.hpp"
-#include "NPC.hpp"
 
 class GameObj_LuaHelper {
 public:
-		//! Default constructor, sets the target to nullptr
-	GameObj_LuaHelper();
-
 		/*!
 		 * @brief Lua GameObject Helper class constructor.
 		 * @note Used for Lua to interact directly with the GameObjects stored within C++.
 		 */
 	GameObj_LuaHelper(std::shared_ptr<GameObj_Base> target);
 
-		//! Checks if the managed GameObject is valid for use by Lua
+		/*!
+		 * @brief Checks if the managed GameObject is valid state for use.
+		 * @return True if the gameObj is safefor use, false otherwise.
+		 */
 	bool gameObj_isValid();
 
+		/*!
+		 * @brief Returns a copy of this GameObject's unique identifier.
+		 * @return An identifier value.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	BlueEngine::ID gameObj_getUniqueID();
 
+		/*!
+		 * @brief Identifies the GameObject's type.
+		 * @return The GameObject's type identifier value.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	GameObjType gameObj_getTypeID();
 
+		/*!
+		 * @brief Tracks and calls for the loading of an external model file.
+		 * @param [in] path The path to a target external model file for this GameObject.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setModel(std::string path);
 
+		/*!
+		 * @brief Returns the model for this GameObject.
+		 * @return The ID of the GameObject's tracked model.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	std::size_t gameObj_getModelID();
 
+		/*!
+		 * @brief Returns the model for this GameObject.
+		 * @return The ID of the tracked model.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	std::string gameObj_getModelPath();
 
+		/*!
+		 * @brief Gathers a copy of the x-coordinate for the GameObject's position in 3D space.
+		 * @return A copy of the GameObject's gameObj_pos x-coordinate.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	float gameObj_getPos_X();
 
+		/*!
+		 * @brief Sets the value of the x-coordinate for the GameObject's position in 3D space.
+		 * @param [in] posX The GameObject's new position x-coordinate value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setPos_X(float posX);
 
+		/*!
+		 * @brief Gathers a copy of the y-coordinate for the GameObject's position in 3D space.
+		 * @return A copy of the GameObject's gameObj_pos y-coordinate.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	float gameObj_getPos_Y();
 
+		/*!
+		 * @brief Sets the value of the y-coordinate for the GameObject's position in 3D space.
+		 * @param [in] posY The GameObject's new position y-coordinate value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setPos_Y(float posY);
 
+		/*!
+		 * @brief Gathers a copy of the z-coordinate for the GameObject's position in 3D space.
+		 * @return A copy of the GameObject's gameObj_pos z-coordinate.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	float gameObj_getPos_Z();
 
+		/*!
+		 * @brief Sets the value of the z-coordinate for the GameObject's position in 3D space.
+		 * @param [in] posZ The GameObject's new position z-coordinate value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setPos_Z(float posZ);
 
+		/*!
+		 * @brief Gathers a copy of the yaw radians for the GameObject's rotation.
+		 * @return A copy of the GameObject's gameObj_yaw value.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	float gameObj_getYaw();
 
+		/*!
+		 * @brief Sets the value of the yaw radians for the GameObject's rotation.
+		 * @param [in] yaw The GameObject's new position gameObj_yaw value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setYaw(float yaw);
 
+		/*!
+		 * @brief Gathers a copy of the pitch radians for the GameObject's rotation.
+		 * @return A copy of the GameObject's gameObj_pitch value.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	float gameObj_getPitch();
 
+		/*!
+		 * @brief Sets the value of the pitch radians for the GameObject's rotation.
+		 * @param [in] pitch The GameObject's new position gameObj_pitch value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setPitch(float pitch);
 
+		/*!
+		 * @brief Gathers a copy of the roll radians for the GameObject's rotation.
+		 * @return A copy of the GameObject's gameObj_roll value.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	float gameObj_getRoll();
 
+		/*!
+		 * @brief Sets the value of the roll radians for the GameObject's rotation.
+		 * @param [in] roll The GameObject's new position gameObj_roll value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setRoll(float roll);
 
+		/*!
+		 * @brief Gathers a copy of the GameObject's physics body identifier.
+		 * @return A copy of the GameObject's gameObj_physBody value.
+		 * @exception std::string Cannot provide return as GameObj_LuaHelper is in an invalid state!
+		 */
 	long unsigned int gameObj_getPhysBody();
 
+		/*!
+		 * @brief Sets the value of the GameObject's physics body identifier.
+		 * @param [in] physBody The GameObject's new position gameObj_physBody value.
+		 * @warning Will silently fail if the LuaHelper is in an invalid state!
+		 */
 	void gameObj_setPhysBody(long unsigned int physBody);
 
 private:
+		//! Stores a smart pointer reference to the GameObject encapsulated.
 	std::shared_ptr<GameObj_Base> gameObj_pointer;
 };
--- src/Model/GameObject/detail/LuaHelper.cpp
+++ src/Model/GameObject/detail/LuaHelper.cpp
@@ -1,89 +1,179 @@
 	/// Declaration Include
 #include "../LuaHelper.hpp"
 
-GameObj_LuaHelper::GameObj_LuaHelper() {
-	// Set an invalid target GameObject
-	gameObj_pointer = nullptr;
-}
-
 GameObj_LuaHelper::GameObj_LuaHelper(std::shared_ptr<GameObj_Base> target) {
 	// Store the target GameObject
 	gameObj_pointer = target;
 }
 
 BlueEngine::ID GameObj_LuaHelper::gameObj_getUniqueID() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_getUniqueID();
 }
 
 GameObjType GameObj_LuaHelper::gameObj_getTypeID() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_getTypeID();
 }
 
 void GameObj_LuaHelper::gameObj_setModel(std::string path) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_setModel(path);
 }
 
 std::size_t GameObj_LuaHelper::gameObj_getModelID() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_getModelID();
 }
 
 std::string GameObj_LuaHelper::gameObj_getModelPath() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_getModelPath();
 }
 
 float GameObj_LuaHelper::gameObj_getPos_X() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_pos[0];
 }
 
 void GameObj_LuaHelper::gameObj_setPos_X(float posX) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_pos[0] = posX;
 }
 
 float GameObj_LuaHelper::gameObj_getPos_Y() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_pos[1];
 }
 
 void GameObj_LuaHelper::gameObj_setPos_Y(float posY) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_pos[1] = posY;
 }
 
 float GameObj_LuaHelper::gameObj_getPos_Z() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_pos[2];
 }
 
 void GameObj_LuaHelper::gameObj_setPos_Z(float posZ) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_pos[2] = posZ;
 }
 
 float GameObj_LuaHelper::gameObj_getYaw() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_yaw;
 }
 
 void GameObj_LuaHelper::gameObj_setYaw(float yaw) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_yaw = yaw;
 }
 
 float GameObj_LuaHelper::gameObj_getPitch() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_pitch;
 }
 
 void GameObj_LuaHelper::gameObj_setPitch(float pitch) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_pitch = pitch;
 }
 
 float GameObj_LuaHelper::gameObj_getRoll() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_roll;
 }
 
 void GameObj_LuaHelper::gameObj_setRoll(float roll) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_roll = roll;
 }
 
 long unsigned int GameObj_LuaHelper::gameObj_getPhysBody() {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		throw std::string("Cannot provide return as GameObj_LuaHelper is in an invalid state!");
+
+	// Perform the operation
 	return gameObj_pointer.get()->gameObj_physBody;
 }
 
 void GameObj_LuaHelper::gameObj_setPhysBody(long unsigned int physBody) {
+	// Catch invalid state of the LuaHelper
+	if (!gameObj_isValid())
+		return;
+
+	// Perform the operation
 	gameObj_pointer.get()->gameObj_physBody = physBody;
 }
 
