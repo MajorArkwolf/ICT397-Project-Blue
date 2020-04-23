336c4f24e7dd17217a07b8a0a363bcab797bcfc4
Peter <petercrabbe92@gmail.com>
2020-04-23 14:06:02 +0800

    Add doxygen to Shader

--- src/View/Renderer/OpenGL.cpp
+++ src/View/Renderer/OpenGL.cpp
@@ -89,7 +89,7 @@ void View::OpenGL::DrawModel(Shader& shader, unsigned int &VAO, const std::vecto
             number = std::to_string(heightNr++); // transfer unsigned int to stream
 
         // now set the sampler to the correct texture unit
-        glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
+        glUniform1i(glGetUniformLocation(shader.getId(), (name + number).c_str()), i);
         // and finally bind the texture
         glBindTexture(GL_TEXTURE_2D, textures[i].id);
     }
