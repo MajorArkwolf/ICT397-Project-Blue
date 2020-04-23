1b2b939aa2c2040ddb2025c44418aa2e172db997
Peter <petercrabbe92@gmail.com>
2020-04-23 14:53:47 +0800

    Add doxygen and optimise Mesh model.

--- src/Model/Models/Mesh.cpp
+++ src/Model/Models/Mesh.cpp
@@ -3,16 +3,16 @@
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include <utility>
 #include "View/Renderer/OpenGL.hpp"
 
 
-Mesh::Mesh(std::vector<Vertex> newVerticies, std::vector<unsigned int> newIndicies,
+Mesh::Mesh(std::vector<Vertex> newVertices, std::vector<unsigned int> newIndices,
            std::vector<TextureB> newTextures) {
-    this->vertices = newVerticies;
-    this->indices  = newIndicies;
-    this->textures = newTextures;
+    this->vertices = std::move(newVertices);
+    this->indices  = std::move(newIndices);
+    this->textures = std::move(newTextures);
     View::OpenGL::SetupMesh(VAO, VBO, EBO, this->vertices, this->indices);
-    //drawItem.shader
 }
 
 void Mesh::Draw(Shader& shader) {
