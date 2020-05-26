37dd83c0ee4aa8b795848b19263cc635fcb9ce71
William Vlahos <wxavierv@gmail.com>
2020-05-26 17:14:20 +0800

    Updated the GameObject factory to reflect the revised GameObject design.

--- src/Controller/Factory/GameAssetFactory.cpp
+++ src/Controller/Factory/GameAssetFactory.cpp
@@ -1,7 +1,10 @@
 #include "GameAssetFactory.hpp"
 
 	/// Internal Dependencies
+#include "Controller/PhysicsManager.hpp"
+#include "Model/GameObject/Types.hpp"
 #include "Model/GameObject/Static.hpp"
+#include "Model/GameObject/Item.hpp"
 #include "Model/GameObject/Player.hpp"
 #include "Model/GameObject/NPC.hpp"
 
@@ -15,25 +18,62 @@ auto Controller::Factory::get() -> Factory & {
     return instance;
 }
 
-std::shared_ptr<GameObj_Base> Controller::Factory::GameObject(GameObjType type) {
+std::shared_ptr<GameObj_Base> Controller::Factory::GameObject(BlueEngine::ID type) {
+	// Keep track of the engine id generator
+	auto id_assigner = BlueEngine::IDTracker::getInstance();
+
 	// Process the provided type key
 	std::shared_ptr<GameObj_Base> object = nullptr;
 	switch (type)
 	{
-	case (GAMEOBJ_STATIC):
-		object = std::make_shared<GameObj_Static>();
-		break;
-	case (GAMEOBJ_PLAYER):
-		object = std::make_shared<GameObj_Player>();
-		break;
-	case (GAMEOBJ_NPC):
-		object = std::make_shared<GameObj_NPC>();
+	case (BlueEngine::ID(GameObj_Type::Static)):
+		{
+			// Create a new physics body for the GameObject, assigining default properties
+			auto phys_id = id_assigner.getID();
+			Physics::PhysicsManager::GetInstance().GetCollisionWorld()->CreateCollisionBody(glm::vec3(0.0f), glm::quat(), phys_id);
+
+			// Create a configured the GameObject
+			object = std::make_shared<GameObj_Static>(0u, phys_id);
+		}
 		break;
 
-	default:
-		// Catch an invalid type
-		object = nullptr;
-		//return nullptr;
+	case (BlueEngine::ID(GameObj_Type::Item)):
+		{
+			// Create a new physics body for the GameObject, assigining default properties
+			auto phys_id = id_assigner.getID();
+			Physics::PhysicsManager::GetInstance().GetCollisionWorld()->CreateCollisionBody(glm::vec3(0.0f), glm::quat(), phys_id);
+
+			// Create a configured the GameObject
+			object = std::make_shared<GameObj_Item>(0u, phys_id);
+		}
+		break;
+
+	case (BlueEngine::ID(GameObj_Type::Player)):
+		{
+			// Create a new physics body for the GameObject, assigining default properties
+			auto phys_id = id_assigner.getID();
+			Physics::PhysicsManager::GetInstance().GetDynamicsWorld()->CreateRigidBody(glm::vec3(0.0f), glm::quat(), phys_id);
+
+			// Create a configured the GameObject
+			object = std::make_shared<GameObj_Player>(0u, phys_id);
+		}
+		break;
+
+	case (BlueEngine::ID(GameObj_Type::NPC)):
+		{
+			// Create a new physics body for the GameObject, assigining default properties
+			auto phys_id = id_assigner.getID();
+			Physics::PhysicsManager::GetInstance().GetDynamicsWorld()->CreateRigidBody(glm::vec3(0.0f), glm::quat(), phys_id);
+
+			//TODO: Finish AI system and generate a FSM for the GameObject
+
+			// Create a configured the GameObject
+			object = std::make_shared<GameObj_NPC>(0u, phys_id, 0u);
+		}
+		break;
+
+	//default:
+		// No need to do anything else, without additional configuration this will always return nullptr.
 	}
 	return object;
 }
