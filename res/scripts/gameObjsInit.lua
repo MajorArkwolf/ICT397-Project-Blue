cda915bd0c58c299cbaae19dc95b2470662e0b75
William Vlahos <wxavierv@gmail.com>
2020-06-10 17:09:47 +0800

    Added all implementation, EXCLUDING MOVEMENT & PHYSICS, for the Wander C++ Regular State. Cleaned up the gameObjsInit script.

--- res/scripts/gameObjsInit.lua
+++ res/scripts/gameObjsInit.lua
@@ -1,35 +1,24 @@
+-- Initialise the physics system, and prepare it for use
 InitialisePhysics();
 physManager = PhysicsManager:GetInstance();
 shapeFactory = physManager:GetReactShapeFactory();
 collisionWorld = physManager:GetCollisionWorld();
 dynamicsWorld = physManager:GetDynamicsWorld();
-
-
 local sphereShape = shapeFactory:createSphere(1);
 local capsuleShape = shapeFactory:createCapsule(1.5,3);
-
--- Load the GameObject Lua Reporting Library functions from script
---dofile("res/scripts/gameObjsReporting.lua");
-
--- Load the GameObject Lua debugging script
---dofile("res/scripts/gameObjsTesting.lua");
-
 
 -- Gather the actual GameObject and configure it
 gameObj_raw = GameObject.getPlayer();
 gameObj_raw.model = resources.getModel("res/model/ball.fbx");
-
 position = vector(0,200,0);
 print(gameObj_raw.model)
 dynamicsWorld:GetRigidBody(gameObj_raw.physBody):SetPosition(position);
 rigidBody = getReactRigidBody(dynamicsWorld:GetRigidBody(gameObj_raw.physBody));
 rigidBody:AddCollisionShape(shapeFactory:GetShape(sphereShape), vector(0,0,0), quaternion(1,0,0,0), 10);
-
 rigidBody:SetBounciness(0);
 
+-- Create character attributes used for moving
 gameObj_charData = GameObject.to_character(gameObj_raw);
-
---Create character attributes used for moving
 gameObj_charData:status_assign("UseDynamics", 0);
 gameObj_charData:status_assign("MoveForward", 0);
 gameObj_charData:status_assign("MoveBackward", 0);
@@ -38,8 +27,7 @@ gameObj_charData:status_assign("MoveRight", 0);
 gameObj_charData:status_assign("Sliding", 0);
 gameObj_charData:status_assign("FreeCam", 0);
 
-
-
+-- Generate 20 NPCs
 for i = 0, 20, 1 do
 	-- Create a GameObject and store the returned identifier
 	gameObj_id = GameObject.create(GameObject.Types.NPC());
@@ -70,4 +58,3 @@ end
 -- Syncronise the physics of the GameObjects after configuring them
 GameObject.syncPhys();
 player = GameObject.getPlayer();
-
--- src/Controller/AI/States/detail/Wander.cpp
+++ src/Controller/AI/States/detail/Wander.cpp
@@ -8,36 +8,65 @@ void State_Wander::start(std::shared_ptr<GameObj_Base> context) {
 
 	// Register the GameObject's statuses for this state
 	std::shared_ptr<GameObj_NPC> npc = std::dynamic_pointer_cast<GameObj_NPC>(context);
-	if (!npc->status_has("Wander_IdleTimeMin")) {
-		// Assign the critical status
-		npc->status_assign("Wander_IdleTimeMin", 2.0f);
-	}
 	if (!npc->status_has("Wander_IdleTimeMax")) {
 		// Assign the critical status
-		npc->status_assign("Wander_IdleTimeMax", 5.0f);
-	}
-	if (!npc->status_has("Wander_WalkTimeMin")) {
-		// Assign the critical status
-		npc->status_assign("Wander_WalkTimeMin", 4.0f);
+		npc->status_assign("Wander_IdleTimeMax", 4.0f);
 	}
 	if (!npc->status_has("Wander_WalkTimeMax")) {
 		// Assign the critical status
-		npc->status_assign("Wander_WalkTimeMax", 6.0f);
+		npc->status_assign("Wander_WalkTimeMax", 9.0f);
 	}
 
 	// Configure the runtime status for the NPC
-	npc->status_assign("Wander_ActionTime", 1.0f);
+	npc->status_assign("Wander_ActionTime", 0.0f);
 	npc->status_assign("Wander_ActionIsIdle", 1.0f);
 }
 
 void State_Wander::run(std::shared_ptr<GameObj_Base> context, double t, double dt) {
+	// Get the entirety of the contextual NPC's properties
+	std::shared_ptr<GameObj_NPC> npc = std::dynamic_pointer_cast<GameObj_NPC>(context);
 
+	// Increment the behaviour timer
+	npc->status_assign("Wander_ActionTime", npc->status_get("Wander_ActionTime") + float(dt));
+
+	// Determine the behaviour to apply for the wandering
+	if (npc->status_get("Wander_ActionIsIdle") < 1.0f) {
+		// The NPC is walking, move it forward
+		//TODO: Implement this!
+
+		// Catch if the wandering behaviour should swap
+		if (npc->status_get("Wander_ActionTime") > npc->status_get("Wander_WalkTimeMax")) {
+			// Change the direction that the AI will face
+			//TODO: Implement this!
+
+			// Change the NPC's animation to walking
+			npc->animator_changeAnimation("IDLE", false);
+
+			// Reset the timer and swap the identifier
+			npc->status_assign("Wander_ActionTime", 0.0f);
+			npc->status_assign("Wander_ActionIsIdle", 1.0f);
+		}
+	}
+	else {
+		// Catch if the wandering behaviour should swap
+		if (npc->status_get("Wander_ActionTime") > npc->status_get("Wander_IdleTimeMax")) {
+			// Change the direction that the AI will face
+			//TODO: Implement this!
+
+			// Change the NPC's animation to walking
+			npc->animator_changeAnimation("WALK", false);
+
+			// Reset the timer and swap the identifier
+			npc->status_assign("Wander_ActionTime", 0.0f);
+			npc->status_assign("Wander_ActionIsIdle", 0.0f);
+		}
+	}
 }
 
-void State_Wander::end(std::shared_ptr<GameObj_Base> context) {
-
+void State_Wander::end(std::shared_ptr<GameObj_Base> context [[maybe_unused]]) {
+	// No behaviour
 }
 
-void State_Wander::read(std::shared_ptr<GameObj_Base> context, BlueEngine::ID msg_sender, Message_Type msg_type, float msg_attachment) {
-
+void State_Wander::read(std::shared_ptr<GameObj_Base> context [[maybe_unused]], BlueEngine::ID msg_sender [[maybe_unused]], Message_Type msg_type [[maybe_unused]], float msg_attachment [[maybe_unused]]) {
+	// No behaviour
 }
